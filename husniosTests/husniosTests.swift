6af3adf72967ee1c3fa95b6f9396d108a3447be6
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2024-10-18 16:57:31 +0530

    Initial Commit

---
+++ husniosTests/husniosTests.swift
@@ -0,0 +1,16 @@
+//
+//  husniosTests.swift
+//  husniosTests
+//
+//  Created by Prashant Shishodia on 18/10/24.
+//
+
+import Testing
+
+struct husniosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
