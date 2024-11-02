e4f43ccbbf9a3cb7f0134b4c6ab775c3205e798d
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2024-11-02 17:33:04 +0530

    fix login screen

--- husnios/Login/LoginScreen.swift
+++ husnios/Login/LoginScreen.swift
@@ -6,50 +6,71 @@ import AuthenticationServices
 // and try to login, it'll take them to onboarding and then quickly to MainScreen
 // This is okay but for now but can be later fixed by keeping state variables isOnboarded/isLoggedIn
 // in ContentView along with appState
+
 struct LoginScreen: View {
     @Binding var appState: AppState
     @Environment(\.colorScheme) var colorScheme
+    @State private var showMoreOptions = false
     
     var body: some View {
         VStack {
             Text("Husn")
                 .font(.custom("Zapfino", size: 72))
-                .foregroundColor(.primary)
+                .foregroundColor(.black)
                 .padding(.vertical, 0)
             //                .padding(.bottom, 50)
             
-            
             VStack {
-                GoogleSignInButton(
-                    scheme:(colorScheme == ColorScheme.dark ? .dark : .light),
-                    style : .wide,
+                Button(
                     action: {
                         handleSignInButton { success in
                             if success {
                                 appState = .Onboarding
                             }
                         }
-                    }).frame(width: 180)
-                    .cornerRadius(12)
+                    }
+                ) {
+                    Image("GoogleSignInButtonDark")
+                        .resizable()
+                        .aspectRatio(contentMode:.fit)
+                        .frame(width: 180)
+
+                }
+                //                .cornerRadius(8)
                 
+                
+                if (!showMoreOptions) {
+                    Button(action: {
+                        showMoreOptions.toggle()
+                    }) {
+                        Text("Show more options >")
+                            .foregroundColor(.white)
+                            .font(.caption)
+                    }
+                    
+                }
+                
+                if showMoreOptions {
+                    SignInWithAppleButton(
+                        onRequest: handleAppleRequest,
+                        onCompletion: { result in
+                            handleAppleSignIn(result) { success in
+                                if success {
+                                    appState = .Onboarding
+                                }
+                            }
+                        }
+                    )
+                    .signInWithAppleButtonStyle(.black)
+                    .frame(width: 180, height: 42)
+                }
             }
             .scaleEffect(x: 1.8, y: 1.8)
-            
-            SignInWithAppleButton(
-                onRequest: handleAppleRequest,
-                onCompletion: { result in
-                    handleAppleSignIn(result) { success in
-                        if success {
-                            appState = .Onboarding
-                        }
-                    }
-                }
-            ).frame(height: 120)
             
             Spacer()
             
         }.background(
-            Image(colorScheme == ColorScheme.dark ? "dark-background" : "light-background")
+            Image("light-background")
                 .resizable()
                 .frame(width: 900, height: 1600)
                 .ignoresSafeArea()
